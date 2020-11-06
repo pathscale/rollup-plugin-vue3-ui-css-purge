8ad0da10fd07e248878dee4f2d0cef2bf6265c97
David <david.cedresx@gmail.com>
2020-11-06 04:48:45 -0400

    fix: bug, update version

--- src/post-cleaner.ts
+++ src/post-cleaner.ts
@@ -1,32 +1,34 @@
 const getDeclaredVariables = (code: string): string[] => {
-    return code.match(/(?<=--)(.*?)(?=:)/g) ?? []
-}
+  return code.match(/(?<=--)(.*?)(?=:)/g) ?? [];
+};
 
 const isVariableUsed = (variable: string, code: string): boolean => {
-    return code.includes(`var(--${variable})`);
-}
-
-const removeLevel = (code: string): string => {
-    const declaredVariables = getDeclaredVariables(code)
-    let _code = code;
-
-    for (const variable of declaredVariables) {
-        if (!isVariableUsed(variable, _code)) {
-            const LINES_WHERE_IS_USED = new RegExp(`.*--${variable}.*;`, "g")
-            _code = _code.replace(LINES_WHERE_IS_USED, "")
-        }
-    }
-    return  _code;
-}
+  return code.includes(`var(--${variable})`);
+};
 
 const cleanHangingVariables = (code: string): string => {
-    const MAX_NESTING = 10;
-    
-    let _code = code;
-    for (let i = 0; i < MAX_NESTING; ++i) {
-        _code = removeLevel(_code)
+  let _code = code;
+  const originalCount = getDeclaredVariables(_code).length;
+  let purgedCount = 0;
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const variables = getDeclaredVariables(_code);
+    const unusedVariables = variables.filter(v => !isVariableUsed(v, _code));
+
+    if (unusedVariables.length === 0) break;
+
+    for (const variable of unusedVariables) {
+      const DECLARATION = new RegExp(`.*--${variable}:.*`, "g");
+      _code = _code.replace(DECLARATION, "");
+
+      if (_code.includes(`--${variable}:`)) throw new Error("hey");
     }
-    return _code;
-}
+    purgedCount += unusedVariables.length;
+  }
+
+  console.log(`purged ${purgedCount} variables out of ${originalCount}`);
+  return _code;
+};
 
 export default cleanHangingVariables;
