2a5dcfee0bd925fdd067d5eaa3d76f7906dfc6f0
Anton Kudryavtsev <boblobl4@gmail.com>
2020-12-24 21:08:53 +0300

    test: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: "node",
   testRunner: "jest-circus/runner",
   transform: { "^.+\\.tsx?$": "ts-jest" },
-  globals: { "ts-jest": { packageJson: "package.json", tsConfig: { target: "es6" } } },
+  globals: { "ts-jest": { tsconfig: { target: "es6" } } },
   testMatch: ["<rootDir>/__tests__/*.(spec|test).[jt]s?(x)"],
   setupFilesAfterEnv: ["<rootDir>/__tests__/setup.ts"],
   collectCoverageFrom: [
@@ -10,5 +10,6 @@ module.exports = {
     "<rootDir>/__tests__/*.[jt]s?(x)",
     "<rootDir>/__tests__/helpers/*.[jt]s?(x)",
     "!**/*.d.ts",
+    "!**/.eslintrc.js",
   ],
 };
