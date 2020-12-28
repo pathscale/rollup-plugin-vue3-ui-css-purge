369efba63744390866ea6489f5d39706ea098bd0
Anton Kudryavtsev <boblobl4@gmail.com>
2020-12-28 03:38:21 +0300

    resetup script upgrade

--- resetup.ps1
+++ resetup.ps1
@@ -1,5 +1,31 @@
-ncu -u --dep 'prod,dev' --concurrency 16 -x 'vue,vue-composable,@vue/compiler-sfc,shadow-flux'
-ncu -u --dep 'prod,dev' --concurrency 16 --target newest -f '@pathscale/vue3-ui,@pathscale/bulma-css-var-only,@pathscale/bulma-pull-2981-css-var-only,vue-router,vuex,rollup-plugin-vue,vue-composable'
+$manager = "pnpm"
+
+$disabledList = @(
+    'vue-composable'
+
+    'shadow-flux'
+)
+
+$nextList = @(
+    'vue-composable'
+
+    'vue'
+    'vuex'
+    'vue-router'
+    'rollup-plugin-vue'
+    '@vue/compiler-sfc'
+
+    '@pathscale/vue3-ui'
+    '@pathscale/bulma-css-var-only'
+    '@pathscale/bulma-extensions-css-var'
+    '@pathscale/bulma-pull-2981-css-var-only'
+)
+
+$disabled = $disabledList -join ","
+$next = $nextList -join ","
+
+ncu -u --dep 'prod,dev' --concurrency 16 -x $disabled
+ncu -u --dep 'prod,dev' --concurrency 16 --target newest -f $next
 sort-package-json
 wsl rm -rf node_modules package-lock.json yarn.lock pnpm-lock.yaml
-pnpm i
+Invoke-Expression $manager i
