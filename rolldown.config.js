60e9d10f1c6bcf108a1c9db53375379c56178ff2
AbsoluteC2H6O <alfredofp63@gmail.com>
2025-04-24 19:10:30 -0400

    roldown(migration): Transform bundle

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,9 +1,28 @@
-import { defineConfig } from 'rolldown';
+import { rolldown } from 'rolldown'
+import nodePolyfills from '@rolldown/plugin-node-polyfills'
+// import vue from "rollup-plugin-vue";
 
-export default defineConfig({
-  input: "src/index.ts",
-  output: {
-    file: 'bundle.js',
-  },
-});
+export default {
+    input: "src/index.ts",
+    output: {
+      // format: 'esm',
+      // dir: 'dist'
+      file: 'bundle.js'
+    },
+    plugins: [
+      {
+        name: 'patch-path-imports',
+        transform(code) {
+          return code.replace(
+            /import\s*\{([^}]*)(win32|posix)([^}]*)\}\s*from\s*['"]path['"]/g,
+            'import { $1 } from "path"'
+          )
+        }
+      }
+    ],
+    platform: 'node',
+    define: {
+      'process.env.NODE_ENV': JSON.stringify('production')
+    },
+}
 
